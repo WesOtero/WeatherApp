69a6486be03212d472eec1449c4f41dafb249866
Wes <wesleyotero@protonmail.com>
2020-07-01 15:48:46 -0400

    Initial commit

---
+++ WeatherAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAPP
+//
+//  Created by Wesley Otero on 7/1/20.
+//  Copyright © 2020 Wesley Otero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
